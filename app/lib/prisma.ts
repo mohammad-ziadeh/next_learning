0c538eab03d1d5f221713927adf8cd2287aab825
mohammad-ziadeh <mohammedziadeh6@gmail.com>
2025-12-14 18:03:51 +0300

    finishing prisma setup and errors

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,20 +1,21 @@
+// lib/prisma.ts
 import { PrismaClient } from "../generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
 const globalForPrisma = global as unknown as {
-    prisma: PrismaClient;
+  prisma: PrismaClient | undefined;
 };
 
-const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL,
+const adapter = new PrismaMariaDb({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  connectionLimit: 5,
 });
 
-const prisma =
-    globalForPrisma.prisma ||
-    new PrismaClient({
-        adapter,
-    });
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export default prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
--- prisma/migrations/20251214150119_init/migration.sql
+++ prisma/migrations/20251214150119_init/migration.sql
@@ -1,8 +1,9 @@
 -- CreateTable
-CREATE TABLE `User` (
+CREATE TABLE `user` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
